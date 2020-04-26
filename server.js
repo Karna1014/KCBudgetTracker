2f759e4cc6e860e2c87772045dd80572ba8a397d
Karna <57597042+Karna1014@users.noreply.github.com>
2020-04-26 14:01:28 -0400

    changed port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 7200;
 
 const app = express();
 
